3c42205a6269dca90213ef6ebb4747169a6672a9
aondrej ryan <aondrejryan11@gmail.com>
2023-02-11 23:58:59 -0800

    refactored code

--- codeWar/findtheneedle.js
+++ codeWar/findtheneedle.js
@@ -1,4 +1,9 @@
 function findNeedle(haystack) {
     // your code here
-   return haystack.includes('needle') ? `found the needle at position ${haystack.indexOf('needle')}` : undefined
+   //return haystack.includes('needle') ? `found the needle at position ${haystack.indexOf('needle')}` : undefined
+   if (haystack.includes('needle')){
+   return  `found the needle at position ${haystack.indexOf('needle')}`
+   } else {
+    return undefined
+   }
   }
