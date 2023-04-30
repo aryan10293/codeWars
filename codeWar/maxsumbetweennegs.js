724cdd72652e5bc88624700219b45d9cea62d169
aondrej ryan <aondrejryan11@gmail.com>
2023-04-29 19:06:29 -0700

    max some bettwen negatves

--- codeWar/maxsumbetweennegs.js
+++ codeWar/maxsumbetweennegs.js
@@ -1,23 +1,22 @@
 function maxSumBetweenTwoNegatives(a) {
-      let arr = []
-      let num1 = 0
-      let num2 = 1
-      let answer = 0
-      a.forEach(( x, i) = > { i f  (x < 0) arr.p ush(i) })
-       if (arr.length <= 1) return -1
-      console.log(ar r, a)
-      f or (let i = 0 ;  i < arr.l e ngth -  1; i++) {
-            let cool = 0
-            let lol = a.slice(arr[num1], arr[num2])
-             for (let j =   0; j < lol.lengt h; j++) {
-                   if (lol[ j] > 0) {
-                          cool += lol[j]
-                  }
-            }
-             if (cool > answer) answer = cool
-            num1++
-            num2++
+  let arr = []
+  let num1 = 0
+  let num2 = 1
+  let answer = 0
+  a.forEach((x,i) => {if (x<0) arr.push(i)})
+  if(arr.length <= 1) return -1
+  console.log(arr,a)
+  for(let i = 0; i<arr.length-1; i++){
+    let cool = 0
+    let lol = a.slice(arr[num1], arr[num2])
+    for(let j = 0; j<lol.length; j++){
+      if(lol[j] > 0){
+        cool+=lol[j]
       }
-      return answer
+    }
+    if(cool > answer) answer = cool
+    num1++
+    num2++
+  }
+  return answer
 }
-axSumBetweenTwoNegatives([1,  -2])
