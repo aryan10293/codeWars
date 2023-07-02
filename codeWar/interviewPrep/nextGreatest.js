59d693d3d3def659414d1239b76b03c437c39ac1
aondrej ryan <aondrejryan11@gmail.com>
2023-07-02 00:28:45 -0700

    next greatest

---
+++ codeWar/interviewPrep/nextGreatest.js
@@ -0,0 +1,9 @@
+var nextGreatestLetter = function(letters, target) {
+    for(let i = 0; i<letters.length;i++){
+        if(letters[i] > target){
+            return letters[i]
+
+        }
+    }
+    return letters[0]
+};
