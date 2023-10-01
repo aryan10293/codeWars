406c0476f603193243cb61d8eddcc38a0e23f67d
aondrej ryan <aondrejryan11@gmail.com>
2023-09-30 23:33:11 -0700

    reviewing maximum sub array

--- codeWar/interviewPrep/maximumSubArray.js
+++ codeWar/interviewPrep/maximumSubArray.js
@@ -47,3 +47,12 @@ var maxSubArray = function(nums) {
     }
     return total
 };
+var maxSubArray = function(nums) {
+    let maxSum = nums[0]
+    let currentSum = nums[0]
+    for(let i = 1; i<nums.length; i++){
+        currentSum = Math.max(nums[i], currentSum + nums[i])
+        maxSum = Math.max(maxSum, currentSum)
+    }
+    return maxSum
+};
