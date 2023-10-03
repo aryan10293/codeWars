6b8e0afefffb8e404c632edf7085ff928b37408e
aondrej ryan <aondrejryan11@gmail.com>
2023-10-03 10:42:20 -0700

    can be equal

---
+++ codeWar/interviewPrep/canBeEqual.js
@@ -0,0 +1,10 @@
+var canBeEqual = function(target, arr) {
+    arr.sort((a,b) => a-b)
+    target.sort((a,b) => a-b)
+    for(let i = 0; i<target.length; i++){
+        if(arr[i] !== target[i]){
+            return false
+        }
+    }
+    return true
+};
