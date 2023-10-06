2153b329105091a27e9f16d955bf69a8048d9cf8
aondrej ryan <aondrejryan11@gmail.com>
2023-10-05 21:57:41 -0700

    is anagram

---
+++ codeWar/interviewPrep/isAnagram.js
@@ -0,0 +1,20 @@
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false
+    let obj = {}
+    for(let i = 0; i<s.length; i++){
+        if(obj[s[i]]){
+           obj[s[i]] = obj[s[i]] + 1 
+        } else {
+            obj[s[i]] = 1
+        }
+    }
+    for(let i = 0; i<t.length; i++){
+        if(obj[t[i]]){
+           obj[t[i]] = obj[t[i]] - 1
+           if(obj[t[i]] === 0) delete obj[t[i]] 
+        } else {
+            return false
+        }
+    }
+    return true
+};
