6f851e4d720505e6e5b339b0715e0d9ec1089d9e
aondrej ryan <aondrejryan11@gmail.com>
2023-10-11 00:08:12 -0700

    summary of ranges

--- codeWar/interviewPrep/summaryOfranges.js
+++ codeWar/interviewPrep/summaryOfranges.js
@@ -26,3 +26,34 @@ var summaryRanges = function(nums) {
     }
     return answer
 };
+
+
+var summaryRanges = function(nums) {
+    let answer = []
+    let count = nums[0]
+    let string = [nums[0]]
+    if(nums.length === 1) return [`${string[0]}`]
+    for(let i = 1; i<nums.length; i++){
+        if(nums[i] === count+1){
+            string.push(nums[i])
+            count = nums[i]
+        } else {
+            if(string.length === 1){
+                console.log(string)
+                answer.push(`${string[0]}`)
+            } else {
+                answer.push(`${string[0]}->${string[string.length - 1]}`)
+            }
+            string = [nums[i]]
+            count = nums[i]
+        }
+        if(nums.length - 1 === i && string.length >= 1){
+            if(string.length === 1){
+                answer.push(`${string[0]}`)
+            } else {
+                answer.push(`${string[0]}->${string[string.length - 1]}`)
+            }
+        }
+    }
+    return answer
+};
