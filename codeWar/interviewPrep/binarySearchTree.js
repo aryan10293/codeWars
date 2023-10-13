d8a11d32e9d30a506b6926540af6eadd27b873fe
aondrej ryan <aondrejryan11@gmail.com>
2023-10-13 13:38:47 -0700

    re did binary tree depth

--- codeWar/interviewPrep/binarySearchTree.js
+++ codeWar/interviewPrep/binarySearchTree.js
@@ -1,12 +1,12 @@
-var maxDepth = function(root) {
-    let max = 0
-    function idk(node, level){
-        if(node === null) return
-        if(level > max) max = level
+const maxDepth = (root) => {
+    let count = 0
+    const idk = (node, level) => {
+        if(node === null) return 
 
+        if(level > count) count = level
+        idk(node.left, level + 1)
         idk(node.right, level + 1)
-        idk(node.left, level+1)
     }
-    idk(root,1)
-    return max
-};
+    idk(root, 1)
+    return count
+}
