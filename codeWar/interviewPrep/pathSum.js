22773af20d1ce263e025fd2cbf951be5067d89a4
aondrej ryan <aondrejryan11@gmail.com>
2023-11-02 12:03:49 -0700

    path sum

---
+++ codeWar/interviewPrep/pathSum.js
@@ -0,0 +1,32 @@
+var hasPathSum = function(root, targetSum) {
+    // obviuosly divide and conquer
+    // if left and right === null
+    // howe tf am i going to keep track of this number
+
+    // const imLost = (num, left,right) => {
+    //     const number = 0 // for sake of visuals
+    //     if(!left && !right && num === targetSum) return true
+    //     if(!left && !right && num !== targetSum) return false
+    //     if(num > targetSum) return false
+    //     return imLost(num+=left.val, left.left, left.right) || imLost(num+=right.val, right.left, right.right)
+    // }
+
+    // return imLost(root.val, root.left, root.right)
+
+    let path = false
+    const imLost = (node, number) => {
+        if(!node) return 
+        
+        number += node.val
+
+        if(!node.left && !node.right && number === targetSum){
+            path = true
+            return
+        }
+        imLost(node.left, number)
+        imLost(node.right, number)
+
+    }
+    imLost(root, 0)
+    return path
+};
