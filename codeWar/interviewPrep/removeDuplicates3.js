4b1de17665d23d0e28be382dbff1e2fed3bf731b
aondrej ryan <aondrejryan11@gmail.com>
2023-11-23 23:21:45 -0800

    removing duplicated part 4 i guess

--- codeWar/interviewPrep/removeDuplicates3.js
+++ codeWar/interviewPrep/removeDuplicates3.js
@@ -10,3 +10,18 @@ var removeDuplicates = function(nums) {
     }
     nums.splice(index)
 };
+
+// i think i get thius problem too
+
+var removeDuplicates = function(nums) {
+    let obj = {}
+    let index = 0
+    for(let i = 0; i<nums.length; i++){
+        if(obj[nums[i]] === undefined){
+            obj[nums[i]] = 1
+            nums[index] = nums[i]
+            index++
+        }
+    }
+    return index
+};
