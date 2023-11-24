f7301f42f4d7746728436e7feac009b38f5b4167
aondrej ryan <aondrejryan11@gmail.com>
2023-11-24 00:46:45 -0800

    this problem is to much for me right now

--- codeWar/interviewPrep/maxProfit.js
+++ codeWar/interviewPrep/maxProfit.js
@@ -13,3 +13,23 @@ var maxProfit = function(prices) {
     }
     return profit
 };
+
+var maxProfit = function(prices) {
+    //
+    let maxProfit = 0
+    let end = 1
+    let minimum = prices[0]
+    for(let i = 1; i<prices.length; i++){
+        if(minimum > prices[i]){
+            minimum = prices[i]
+            end++
+        } else {
+            let currentProfit = prices[end] - minimum
+            if(currentProfit !== NaN && currentProfit > maxProfit){
+                maxProfit = currentProfit
+            }
+            end++
+        }
+    }
+    return maxProfit
+};
