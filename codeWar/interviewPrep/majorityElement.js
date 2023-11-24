f86be96c1643f790fddafead9f3045604c31d009
aondrej ryan <aondrejryan11@gmail.com>
2023-11-23 23:50:55 -0800

    majority element

--- codeWar/interviewPrep/majorityElement.js
+++ codeWar/interviewPrep/majorityElement.js
@@ -13,3 +13,23 @@ var majorityElement = function(nums) {
     }
     return answer
 };
+
+// def get this problem
+
+var majorityElement = function(nums) {
+    let obj = {}
+    let count = [0,0]
+    for(let i = 0; i<nums.length; i++){
+        if(obj[nums[i]] === undefined){
+            obj[nums[i]] = 1
+        } else {
+            obj[nums[i]] = obj[nums[i]] + 1
+        }
+    }
+    for(let key in obj) {
+        if(obj[key] > count[0]){
+            count = [obj[key], key]
+        }
+    }
+    return count[1]
+};
