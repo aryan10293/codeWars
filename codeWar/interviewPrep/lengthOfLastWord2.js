e3057a9028d1918f50601a2eac0a086ba71ab5f6
aondrej ryan <aondrejryan11@gmail.com>
2023-11-24 00:52:20 -0800

    i did the same thing lol

--- codeWar/interviewPrep/lengthOfLastWord2.js
+++ codeWar/interviewPrep/lengthOfLastWord2.js
@@ -3,3 +3,8 @@ var lengthOfLastWord = function(s) {
     s = s.split(' ')
     return s[s.length - 1].length
 };
+var lengthOfLastWord = function(s) {
+    s = s.trim()
+    s = s.split(' ')
+    return s[s.length - 1].length
+};
