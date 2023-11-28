9a384ef16c332d7836dfa48253d10791c6d200bb
aondrej ryan <aondrejryan11@gmail.com>
2023-11-28 02:21:17 -0800

    can construct

--- codeWar/interviewPrep/canConstruct.js
+++ codeWar/interviewPrep/canConstruct.js
@@ -19,3 +19,32 @@ var canConstruct = function(ransomNote, magazine) {
     }
     return true
 };
+
+// the second time doing this one kinda coooked me 
+
+var canConstruct = function(ransomNote, magazine) {
+    let rObj = {}
+    let mObj = {}
+    let count = 0
+    for(let i = 0; i<ransomNote.length; i++){
+        if(!rObj[ransomNote[i]]){
+            rObj[ransomNote[i]] = 1
+        } else {
+            rObj[ransomNote[i]] = rObj[ransomNote[i]] + 1
+        }
+    }
+
+    for(let i = 0; i<magazine.length; i++){
+        if(!mObj[magazine[i]]){
+            mObj[magazine[i]] = 1
+        } else {
+            mObj[magazine[i]] = mObj[magazine[i]] + 1
+        }
+    }
+
+    for (const key in rObj) {
+        console.log(rObj, mObj)
+        if(rObj[key] <= mObj[key]) count++
+    }
+    return count === Object.values(rObj).length
+};
