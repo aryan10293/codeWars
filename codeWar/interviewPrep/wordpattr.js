9dfa99ad2881575c946565e85a52ee07655a3a30
aondrej ryan <aondrejryan11@gmail.com>
2023-11-28 23:19:07 -0800

    word pattern

--- codeWar/interviewPrep/wordpattr.js
+++ codeWar/interviewPrep/wordpattr.js
@@ -17,3 +17,44 @@ var wordPattern = function(pattern, s) {
     }
     return true
 };
+
+// i have no idea what i was doing above and i didnt understand months after below is my new trash code
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let pObj = {}
+    let sObj = {}
+    s = s.split(' ')
+    if(pattern.split('').length !== s.length) return false
+    for(let i = 0; i<pattern.length; i++){
+        if(!pObj[pattern[i]]){
+           pObj[pattern[i]] = [i] 
+        } else {
+            pObj[pattern[i]].push(i)
+        }
+    }
+    for(let i = 0; i<s.length; i++){
+        if(!sObj[s[i]]){
+           sObj[s[i]] = [i] 
+        } else {
+            sObj[s[i]].push(i)
+        }
+    }
+    pObj = Object.values(pObj)
+    sObj = Object.values(sObj)
+    console.log(pObj, sObj)
+    if(pObj.length !== sObj.length) return false
+    for (let i = 0; i < pObj.length; i++) {
+        for (let j = 0; j < pObj[i].length; j++) {
+            // Compare values at the same position
+            if (pObj[i][j] !== sObj[i][j]) {
+                return false
+            }
+        }
+    }
+    return true
+};
