cba2c3b049a88252ab5dc370a9bd6564179ed58e
aondrej ryan <aondrejryan11@gmail.com>
2023-11-28 23:51:01 -0800

    contains near by dup

--- codeWar/interviewPrep/containsNearbyDup.js
+++ codeWar/interviewPrep/containsNearbyDup.js
@@ -16,3 +16,24 @@ var containsNearbyDuplicate = function(nums, k) {
     }
     return false
 };
+
+// solution is slow but i get the problem  if asked to optimized id be cooked
+
+var containsNearbyDuplicate = function(nums, k) {
+    let obj = {}
+    for(let i = 0; i<nums.length; i++){
+        if(obj[nums[i]]){
+           obj[nums[i]].push(i) 
+        } else {
+            obj[nums[i]] = [i]
+        }
+    }
+    obj = Object.values(obj).filter(x => x.length > 1)
+    for(let i = 0; i<obj.length; i++){
+        for(let j = 0; j<obj[i].length; j++){
+            if(Math.abs(obj[i][j] - obj[i][j + 1]) <= k) return true
+
+        }
+    }
+    return false
+};
