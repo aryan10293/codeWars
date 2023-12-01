4e08c92c5c29c0694d11b6c059e223fc3b22f9fb
aondrej ryan <aondrejryan11@gmail.com>
2023-12-01 01:13:09 -0800

    two some

--- codeWar/twosum.js
+++ codeWar/twosum.js
@@ -13,3 +13,24 @@ function twoSum(numbers, target) {
       }
     }
   }
+
+  /// this shit easy]
+
+  var twoSum = function(nums, target) {
+    // create a object to hold number
+    // insid the object you want to hold the number and its in dices
+    // as you loop through the array check to see if the difference is in the object
+    // if the difference is in the object reuturn true 
+
+    // below is gfoign to be a obj that holds the numbers and its indices
+    let obj = {}
+
+    for(let i = 0; i<nums.length; i++){
+        let cool = target - nums[i]
+        if(obj[cool]){
+            return [i, obj[cool][0]]
+        } else {
+            obj[nums[i]] = [i]
+        }
+    }
+};
