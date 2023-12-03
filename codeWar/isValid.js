816fbc7f91339dd3dc2853ea28772708d56baa29
aondrej ryan <aondrejryan11@gmail.com>
2023-12-03 13:26:05 -0800

    is valid parene

---
+++ codeWar/isValid.js
@@ -0,0 +1,22 @@
+var isValid = function(s) {
+    // 
+    let answer = []
+    let topOfArray = '[ { ('
+    let validBracket = '{} [] ()'
+
+    for(let i = 0; i<s.length; i++){
+        if(topOfArray.includes(s[i])){
+            answer.push(s[i])
+        } else {
+            let isValidBracket = `${answer[answer.length - 1]}${s[i]}`
+            if(validBracket.includes(isValidBracket)){
+                answer.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return answer.length === 0 
+
+
+};
