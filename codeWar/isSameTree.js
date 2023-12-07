4227344726c7b312b370277dae08de4f62c68cd0
aondrej ryan <aondrejryan11@gmail.com>
2023-12-07 13:38:49 -0800

    is same tree

---
+++ codeWar/isSameTree.js
@@ -0,0 +1,12 @@
+var isSameTree = function(p, q) {
+    if(p === null && q === null) return true
+    if(p === null || q === null) return false
+
+    if(p.val === q.val){
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+
+    return false
+};
+
+// i get the question i kinda get the code  
