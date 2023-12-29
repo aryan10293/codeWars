4d7a81d82a9a1f7d678421094df57703b02fe9c4
aondrej ryan <aondrejryan11@gmail.com>
2023-12-29 12:04:24 -0800

    jus tryna get better

--- codeWar/interviewPrep/isValidBST.js
+++ codeWar/interviewPrep/isValidBST.js
@@ -1,11 +1,11 @@
-var isValidBST = function(root) {
+var isValidBST = function (root) {
     let isValid = true
     const idk = (node, side) => {
-        if(!node ) return 
-        if(node.left && node.left.val >= node.val) isValid = false
-        if(node.right && node.right.val <= node.val) isValid = false
-        if(node.left && side === 'right' && node.left.val <= root.val) isValid = false
-        if(node.right && side === 'left' && node.right.val >= root.val) isValid = false
+        if (!node) return
+        if (node.left && node.left.val >= node.val) isValid = false
+        if (node.right && node.right.val <= node.val) isValid = false
+        if (node.left && side === 'right' && node.left.val <= root.val) isValid = false
+        if (node.right && side === 'left' && node.right.val >= root.val) isValid = false
         idk(node.left, 'left')
         idk(node.right, 'right')
     }
@@ -14,12 +14,12 @@ var isValidBST = function(root) {
 };
 
 // below is the correct code
-
-var isValidBST = function(root) {
+// signed up for school
+var isValidBST = function (root) {
     const idk = (node, min, max) => {
-        if(!node) return true
+        if (!node) return true
 
-        if(node.val >= max || node.val <= min) return false
+        if (node.val >= max || node.val <= min) return false
 
         return idk(node.left, min, node.val) && idk(node.right, node.val, max)
     }
