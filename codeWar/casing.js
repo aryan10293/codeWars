3d317736ae3f1e47316fb947d9c4953a319b9765
aondrej ryan <aondrejryan11@gmail.com>
2024-01-03 23:25:35 -0800

    get better day by day. lol idk

--- codeWar/casing.js
+++ codeWar/casing.js
@@ -19,3 +19,4 @@ function capitalize(s){
   // drej needs to get better 
   // drej got a little better
   // im getting a rythem again  just gotta start working on my preject and doing some leetcode but everything else is going good
+  // lit two off days. started class. filmed two tiktoks 3 tiktok posted 1. did little homework, wasnt to hard lol. uhhhh still gotta work on my project though
