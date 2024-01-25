2c80a3f122c30cf9fef65ee8881b03cf66dab99f
aondrej ryan <aondrejryan11@gmail.com>
2024-01-25 00:57:57 -0800

    coding hoemwork

--- codeWar/interviewPrep/homework.py
+++ codeWar/interviewPrep/homework.py
@@ -1,5 +1,6 @@
 product = input("What is the name of the product you're buying?")
-price = float(input("How much is the product you are buying? Be honest.... please!"))
+price = float(
+    input("How much is the product you are buying? Be honest.... please!"))
 quanity = int(input("How many are you buying? once again please be honest!!!"))
 weight = float(input("How much does one of these weight????????"))
 
@@ -13,3 +14,53 @@ print("The subtotal is $" + str(subtotal))
 print("The Shipping and Handling costs are $" + str(shipping))
 print("The Tax is $" + str(tax))
 print("The Total is $" + str(total))
+
+# 11.10 question 1
+# filename = 'studentdata.txt'
+# with open(filename, 'r') as file_object:
+# Read the entire content of the file into a string
+#  content = file_object.read()
+#  print(content)
+
+infile = open("studentdata.txt", "r")
+line = infile.readline()
+while line:
+    values = line.split()
+    # print('In', values[0], 'the average temp. was', values[1], '°C and CO2 emmisions were', values[2], 'gigatons.')
+    # print(values)
+    line = infile.readline()
+    if len(values) > 7:
+        print(values[0])
+
+infile.close()
+
+# question 2
+
+infile = open("studentdata.txt", "r")
+line = infile.readline()
+while line:
+    values = line.split()
+    line = infile.readline()
+    scores = values[1:]
+    name = values[0]
+    total_score = 0
+    for i in scores:
+        total_score += int(i)
+    print(name, round(total_score/len(scores), 2))
+
+    # question 3
+    infile = open("studentdata.txt", "r")
+line = infile.readline()
+while line:
+    values = line.split()
+    line = infile.readline()
+    scores = values[1:]
+    name = values[0]
+    max = 0
+    min = float('inf')
+    for i in scores:
+        if max < int(i):
+            max = int(i)
+        if min > int(i):
+            min = int(i)
+    print(name, min, max)
