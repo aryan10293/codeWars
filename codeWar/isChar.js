8d6a9c174e90067428f6ac1a9448fffc57ff487d
aondrej ryan <aondrejryan11@gmail.com>
2024-03-24 12:03:10 -0700

    on my way to canada

---
+++ codeWar/isChar.js
@@ -0,0 +1,4 @@
+function isItLetter(character) {
+  const letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  return letter.includes(character.toLowerCase());
+}
