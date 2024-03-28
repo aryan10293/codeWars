e468afaa7ad0626578f5899fd90c66041df5458f
aondrej ryan <aondrejryan11@gmail.com>
2024-03-28 12:32:10 -0700

    bus stops

--- codeWar/busStops.js
+++ codeWar/busStops.js
@@ -5,3 +5,13 @@ var number = function(busStops){
   busStops.forEach(a => cool += a[1])
   return answer - cool
 }
+
+var number = function(busStops){
+  // Good Luck!
+  let total = 0
+  for(let i = 0; i<busStops.length; i++){
+    total += busStops[i][0]
+    total -= busStops[i][1]
+  }
+  return total
+}
