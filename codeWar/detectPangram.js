bd7598f823c689bc53144d0643311fb8f3fe291a
aondrej ryan <aondrejryan11@gmail.com>
2024-04-13 23:02:54 -0700

    10 walk

--- codeWar/detectPangram.js
+++ codeWar/detectPangram.js
@@ -9,3 +9,22 @@ function isPangram(string){
     }
     return true 
   }
+
+  function isPangram(string){
+  //...
+  string = string.toLowerCase()
+  let lol = 'qwertyuioplkjhgsazxcvbnm'
+  let obj = {}
+  for(let i = 0; i<lol.length; i++){
+    obj[lol[i]] = false
+  }
+  
+  for(let i in string){
+    obj[string[i]] = true
+  }
+  
+  for(let key in obj){
+    if(obj[key] === false) return false
+  } 
+  return true
+}
