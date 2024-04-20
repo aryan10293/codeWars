63349d9b5034c21e6e673110db7a3ea42e25c0b4
aondrej ryan <aondrejryan11@gmail.com>
2024-04-19 23:22:57 -0700

    find outliers

--- codeWar/testing.js
+++ codeWar/testing.js
@@ -2,3 +2,8 @@ var number=function(array){
     //your awesome code here
     return array.map((x,i) => {return `${i + 1}: ${x}`})
   }
+
+var number = function (array) {
+  //your awesome code here
+  return array.map((x, i) => `${i + 1}: ${x}`)
+}
