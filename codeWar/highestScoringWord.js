fcc7eb21b4a5ac8fcb5151705ddd26c2c8f28305
aondrej ryan <aondrejryan11@gmail.com>
2024-04-23 10:20:16 -0700

    find outliers

--- codeWar/highestScoringWord.js
+++ codeWar/highestScoringWord.js
@@ -15,6 +15,27 @@ function high(x){
     })
     return high
   }
-high('aaa ')
-high('d bb')
-high('bb d')
+function high(x){
+  x = x.split(' ')
+  const alphabet = {
+    a: 1, b: 2, c: 3, d: 4, e: 5,
+    f: 6, g: 7, h: 8, i: 9, j: 10,
+    k: 11, l: 12, m: 13, n: 14, o: 15,
+    p: 16, q: 17, r: 18, s: 19, t: 20,
+    u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+  };
+  
+  let score = 0
+  let word;
+  for(let i in x){
+    let count = 0
+    for(let j in x[i]){
+      count += alphabet[x[i][j]]
+    }
+    if(count > score){
+      word = x[i]
+      score = count
+    }
+  }
+  return word
+}
