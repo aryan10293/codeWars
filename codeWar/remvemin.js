84141375a2019caf87e8e392c3d61136c93f7165
aondrej ryan <aondrejryan11@gmail.com>
2024-04-24 14:10:29 -0700

    find outliers

--- codeWar/remvemin.js
+++ codeWar/remvemin.js
@@ -1,12 +1,20 @@
 function removeSmallest(numbers) {
-    //   return [];
-      let lol = []
-      let num = numbers.filter((a) => {
-        if(a === Math.min(...numbers) && !lol.includes(a)){
-          lol.push(a)
-        } else {
-          return a
-        }
-      })
-    return num
+  //   return [];
+  let lol = []
+  let num = numbers.filter((a) => {
+    if (a === Math.min(...numbers) && !lol.includes(a)) {
+      lol.push(a)
+    } else {
+      return a
     }
+  })
+  return num
+}
+
+function removeSmallest(numbers) {
+  //console.log(numbers.indexOf(Math.min(...numbers)))
+  let lol = []
+  const index = numbers.indexOf(Math.min(...numbers))
+  numbers.forEach((x,  i) =>  i  != index ? lol.push(x) : null)
+  return lol
+}
