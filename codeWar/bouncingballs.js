77d71ae03ab995abb3cfe15ee4e40ec8e4781c07
aondrej ryan <aondrejryan11@gmail.com>
2024-04-30 10:31:47 -0700

    find outliers

--- codeWar/bouncingballs.js
+++ codeWar/bouncingballs.js
@@ -1,10 +1,22 @@
-function bouncingBall(h,  bounce,  window) {
-    // your code here
-    let answer = 0
-    if(h<=0 || (bounce <= 0 || bounce >= 1) || window >= h) return -1
-    for(let i = 1; h>window; i++){
-      h = h*bounce
-      answer++
-    }
-    return answer*2-1
+function bouncingBall(h, bounce, window) {
+  // your code here
+  let answer = 0
+  if (h <= 0 || (bounce <= 0 || bounce >= 1) || window >= h) return -1
+  for (let i = 1; h > window; i++) {
+    h = h * bounce
+    answer++
   }
+  return answer * 2 - 1
+}
+
+function bouncingBall(h, bounce, window) {
+  // your code here
+  let count = 0
+  if (h <= 0 || (bounce <= 0 || bounce >= 1) || window >= h) return -1
+  while (h > window) {
+    count++
+    h = h * .66
+    if (h > window) count++
+  }
+  return count
+}
