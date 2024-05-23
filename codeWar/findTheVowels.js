2cebcf6b0cd764036119161ed96858908cf427db
aondrej ryan <aondrejryan11@gmail.com>
2024-05-22 18:10:40 -0700

    remove duplicates

---
+++ codeWar/findTheVowels.js
@@ -0,0 +1,9 @@
+function vowelIndices(word){
+  //your code here
+  const vowels = 'aeiou'
+  const lol = []
+  for(let i = 0; i<word.length; i++){
+    vowels.includes(word[i]) ? lol.push(i+1) : null
+  } 
+  return lol
+}
