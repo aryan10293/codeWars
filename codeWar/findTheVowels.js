55b7c9311ee9f652f4d489514657d4c60c7850e5
aondrej ryan <aondrejryan11@gmail.com>
2024-05-22 18:12:55 -0700

    remove duplicates

--- codeWar/findTheVowels.js
+++ codeWar/findTheVowels.js
@@ -1,6 +1,6 @@
 function vowelIndices(word){
   //your code here
-  const vowels = 'aeiou'
+  const vowels = 'aeiouAEIOUyY'
   const lol = []
   for(let i = 0; i<word.length; i++){
     vowels.includes(word[i]) ? lol.push(i+1) : null
