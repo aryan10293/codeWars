f3d34962dbcb3f2196af9d8112d178e1a38e3222
aondrej ryan <aondrejryan11@gmail.com>
2024-08-03 23:53:30 -0700

    vowel count

---
+++ codeWar/findmissingnumber.py
@@ -0,0 +1,16 @@
+def missing_no(nums):
+    nums.sort()
+    print(nums)
+    for i in range(len(nums)):
+        print(i+1, len(nums))
+        if i+1 == len(nums):
+            if nums[i] + 1 == 101:
+                return 0
+            else:
+                return nums[i] + 1
+        next_num = nums[i+1]
+        if next_num != nums[i] + 1:
+            return nums[i] + 1
+
+    #  Your code her
+e
